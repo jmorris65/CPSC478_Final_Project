7a9e084fccf1cb04ea37baff613a252cff9b5dcf
Morris Jalil <jim33@zoo.cs.yale.edu>
2021-04-25 17:54:41 -0400

    Added some scene geometry

--- main.cpp
+++ main.cpp
@@ -9,17 +9,19 @@ int main (int argc, char **argv)
   // Constants
   char name[256];
   char root[] = "test";
-  VEC3 eye (0.0, 1.0, 1.5);
+  VEC3 eye (3.0, 8.0, 3.0);
   VEC3 lookAt (0.0, 1.0, 0.0);
   VEC3 u (0.0, 1.0, 0.0);
 
   int x = 500;
   int y = 500;
   int depthLimit = 10;
-  loadSkeleton ("wobble.asf", "wobble.amc", 5, 1900, 300);
+  // 1850
+  loadSkeleton ("wobble.asf", "wobble.amc", 5, 2275, 300);
 
   int i;
   Actor *a;
+  float height, width, dist, zx, yt, stride;
 
   for (i = 0; i < 1; i++) {
     setLookAt (eye, lookAt, u);
@@ -39,7 +41,29 @@ int main (int argc, char **argv)
     a = new Point (VEC3 (-5.0, 1.0, -6.0));
     makeLight (a, VEC3 (1.0, 1.0, 1.0), 1.0);
 
-    makeSquare (10.0, 10.0, 1, 1, VEC3 (0.0, 0.0, 0.0), VEC3 (0.0, 0.0, 0.0)); 
+    height = 3.0;
+    width = 5.5;
+    dist = (10.0 - width) / 2.0;
+    zx = sqrt ((dist * dist) / 2.0);
+    yt = height / 2.0;
+    stride = 0.2;
+    makeSquare (height, width, 1, 1, VEC3 (zx + stride, yt, -1.0 * zx + stride), VEC3 (90.0, 45.0, 0.0));
+    
+    width = 3.5;
+    dist = (10.0 - width) / 2.0;
+    zx = sqrt ((dist * dist) / 2.0);
+    stride = 0.2;
+    makeSquare (height, width, 1, 1, VEC3 (-1.0 * zx + stride, yt, zx + stride), VEC3 (90.0, 45.0, 0.0));
+
+    height = 1.0;
+    width = 1.0;
+    dist = 1.0;
+    zx = sqrt ((dist * dist) / 2.0);
+    yt = (height / 2.0) + 2.0;
+    stride = 0.2;
+    makeSquare (height, width, 1, 1, VEC3 (-1.0 * zx + stride, yt, zx + stride), VEC3 (90.0, 45.0, 0.0));
+
+    makeSquare (10.0, 10.0, 1, 1, VEC3 (0.0, 0.0, 0.0), VEC3 (0.0, 45.0, 0.0)); 
 
     sprintf (name, "./Frames/%s.%04d.ppm", root, i);
     makeFrame (string (name));
